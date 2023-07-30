24710148ad0fb75b6946e64a2a6cc9eb0c8a9155
daridev <darideveloper@gmail.com>
2023-07-30 15:22:37 -0600

    feat: initial repo

---
+++ api.py
@@ -0,0 +1,15 @@
+
+
+class Api ():
+    
+    def __init__ (self):
+        self.token = None
+    
+    def login (self):
+        pass
+    
+    def get_projects (self):
+        pass
+    
+    def get_markdown (self):
+        pass
