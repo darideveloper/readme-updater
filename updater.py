23156d43dae87f8cf35b44cd5f6c7143efa7ae16
daridev <darideveloper@gmail.com>
2023-07-31 15:47:16 -0600

    feat (update): pull before update

--- updater.py
+++ updater.py
@@ -27,6 +27,7 @@ def update_readme (path:str, markdown:str):
     path_formatted = path.replace ('\\', '/')
     print ("---------- Git ----------")
     os.system (f'git config --global --add safe.directory {path_formatted}')
+    os.system ('git pull origin master')
     os.system ('git add README.md')
     os.system ('git commit -m "Update README.md with Bot"')
     os.system ('git push origin master')
