2a4b10cc7ca065c2082724d44c126313c3e7c9dd
daridev <darideveloper@gmail.com>
2023-07-30 18:16:08 -0600

    feat (api): debug mode

--- api.py
+++ api.py
@@ -7,12 +7,16 @@ load_dotenv ()
 API_USER_NAME = os.getenv ('API_USER_NAME')
 API_PASSWORD = os.getenv ('API_PASSWORD')
 API_BASE = os.getenv ('API_BASE')
+DEBUG = os.getenv ('DEBUG') == 'True'
 
 class Api ():
     
     def __init__ (self):
         self.token = None
         self.projects_data = {}
+        
+        if DEBUG: 
+            print (">>Debug mode<<")
     
     def __login__ (self):
         """ Login with crdentials and get token """
@@ -40,7 +44,7 @@ class Api ():
     def __query_projects__ (self):
         """ Get update status of all projects """
         
-        print ("Getting projects data...")
+        print ("\nGetting projects data...")
                 
         url = f"{API_BASE}/projects-sumary/"
         while True:
@@ -63,7 +67,7 @@ class Api ():
                 if not updated_remote and location_pc:
                     self.projects_data[id] = {
                         'name': name,
-                        'location_pc': location_pc
+                        'location': location_pc
                     }
             
             # Show status
@@ -71,7 +75,8 @@ class Api ():
             print (f"\t{projects_count} projects found")
             
             # Debug
-            break
+            if DEBUG:
+                break
             
             # End loop if no more pages
             if not url:
@@ -80,7 +85,7 @@ class Api ():
     def __query_markdown__ (self):
         """ get markdown data of all projects """
         
-        print ("Getting markdown data...")
+        print ("\nGetting markdown data...")
         
         # Update each project
         for project_id, project_data in self.projects_data.items():
@@ -101,6 +106,9 @@ class Api ():
             res_json = res.json ()
             markdown = res_json['data']
             
+            # remove backslashes
+            markdown = markdown.replace ('\\', '')
+            
             # Save markdown in dict
             self.projects_data[project_id]["markdown"] = markdown
             
@@ -117,6 +125,8 @@ class Api ():
         return self.projects_data        
     
 if __name__ == '__main__':
+    
+    # Debug api when run directly
     api = Api ()
     projects_data = api.get_data ()
     print ()
