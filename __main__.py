d46f6004ff112831c106c867f66c8412c83b0552
daridev <darideveloper@gmail.com>
2023-07-30 18:48:12 -0600

    feat: update readme and push

--- __main__.py
+++ __main__.py
@@ -1,13 +1,27 @@
 import os
-from dotenv import load_dotenv
-from logs import logger
-from scraping_manager.automate import WebScraping
-
-load_dotenv ()
-VAR = os.getenv ("VAR")
+from api import Api
+from updater import update_readme
 
 def main (): 
-
-
+    
+    apì = Api ()
+    projects_data = apì.get_data ()
+    
+    print ("Upating projects...")
+    for project_id, project_data in projects_data.items ():
+        
+        # Get project data
+        project_name = project_data['name']
+        project_location = project_data['location']
+        markdown = project_data.get ('markdown', None)
+        
+        # Validate markdown
+        if not markdown:
+            print (f"\tNo markdown data for project '{project_name}'")
+            continue
+        
+        print (f"\tUpdating '{project_name}' in path '{project_location}'...")
+        update_readme (project_location, markdown)
+    
 if __name__ == "__main__":
     main()
--- updater.py
+++ updater.py
@@ -1,10 +1,35 @@
-# print (f"\tUpdating '{project_name}' in path '{project_location}'...")
+import os
+
+def update_readme (path:str, markdown:str):
+    """ Update README.md file in a project and push changes
+
+    Args:
+        path (str): path to project
+        markdown (str): markdown data to write in README.md file
+    """
+    
+    # Move to project location
+    os.chdir (path)
+
+    # Create readme file if not exists
+    files = os.listdir ()
+    if 'README.md' not in files:
+        with open ('README.md', 'w') as file:
+            file.write ('')
+        print (f"\t\tREADME.md file created")    
+    
+    # Write data in readme file 
+    with open ('README.md', 'w', encoding='UTF-8') as file:
+        file.write (markdown)
+    print (f"\t\tREADME.md file updated")
         
-        # # Move to project location
-        # os.chsdir (project_location)
-        
-        # # Validate if there a is a README.md file
-        # files = os.listdir ()
-        # if 'README.md' not in files:
-        #     print ("\t\tNo README.md file found")
-        #     continue    
+    # Commit changes
+    path_formatted = path.replace ('\\', '/')
+    print ("\n---------- Git ----------")
+    os.system (f'git config --global --add safe.directory {path_formatted}')
+    os.system ('git add .')
+    os.system ('git commit -m "Update README.md with Bot"')
+    os.system ('git push origin master')
+    print ("--------------------------\n")
+    
+    print (f"\t\tDone.")
